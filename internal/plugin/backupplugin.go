df4047969bf82a195f293525307eaecc90b7da48
m-motawea <magedmotawea@gmail.com>
2021-02-25 17:17:01 +0200

    print item

--- internal/plugin/backupplugin.go
+++ internal/plugin/backupplugin.go
@@ -19,48 +19,27 @@ package plugin
 import (
 	"github.com/sirupsen/logrus"
 
-	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime"
 
 	v1 "github.com/vmware-tanzu/velero/pkg/apis/velero/v1"
 	"github.com/vmware-tanzu/velero/pkg/plugin/velero"
 )
 
-// BackupPlugin is a backup item action plugin for Velero.
 type BackupPlugin struct {
 	log logrus.FieldLogger
 }
 
-// NewBackupPlugin instantiates a BackupPlugin.
 func NewBackupPlugin(log logrus.FieldLogger) *BackupPlugin {
 	return &BackupPlugin{log: log}
 }
 
-// AppliesTo returns information about which resources this action should be invoked for.
-// A BackupPlugin's Execute function will only be invoked on items that match the returned
-// selector. A zero-valued ResourceSelector matches all resources.
 func (p *BackupPlugin) AppliesTo() (velero.ResourceSelector, error) {
 	return velero.ResourceSelector{}, nil
 }
 
-// Execute allows the ItemAction to perform arbitrary logic with the item being backed up,
-// in this case, setting a custom annotation on the item being backed up.
 func (p *BackupPlugin) Execute(item runtime.Unstructured, backup *v1.Backup) (runtime.Unstructured, []velero.ResourceIdentifier, error) {
-	p.log.Info("Hello from my BackupPlugin!")
-
-	metadata, err := meta.Accessor(item)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	annotations := metadata.GetAnnotations()
-	if annotations == nil {
-		annotations = make(map[string]string)
-	}
-
-	annotations["velero.io/my-backup-plugin"] = "1"
-
-	metadata.SetAnnotations(annotations)
+	p.log.Info("backing up")
+	p.log.Info(item)
 
 	return item, nil, nil
 }
